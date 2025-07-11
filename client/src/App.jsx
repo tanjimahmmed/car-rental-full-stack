0f0f6bd4f2b097a9d145fc241e0e3820b1eb66a9
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-07-11 21:15:13 +0600

    add navbar for all pages and add owner path for dashboard panel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from './components/Navbar'
+import { useLocation } from 'react-router-dom';
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
+  const isOwnerPath = useLocation().pathname.startsWith('/owner')
+
   return (
-    <div className='text-5xl font-bold underline'>App</div>
+    <>
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
+    </>
   )
 }
 
