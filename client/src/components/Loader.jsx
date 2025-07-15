b253bffa359412aaf4488e6691b8635a520d1471
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-07-15 16:57:57 +0600

    add carDetails page with loader component

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className='flex justify-center items-center h-[8vh]'>
+        <div className="animate-spain rounded-full h-14 w-14 border-4 border-gray-300 border-t-primary">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Loader
