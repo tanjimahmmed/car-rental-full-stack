11469c7fb19937dc8ea81561837d9f5c2a96f017
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-07-17 21:27:18 +0600

    add dashboard panel side bar with nav link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,11 @@ import Home from './pages/Home';
 import Cars from './pages/Cars';
 import MyBookings from './pages/MyBookings';
 import Footer from './components/Footer';
+import Layout from './pages/owner/Layout';
+import Dashboard from './pages/owner/Dashboard';
+import AddCar from './pages/owner/AddCar';
+import ManageBookings from './pages/owner/ManageBookings';
+import ManageCars from './pages/owner/ManageCars';
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
@@ -20,6 +25,12 @@ const App = () => {
         <Route path="/car-details/:id" element={<CarDetails/>}/>
         <Route path="/cars" element={<Cars/>}/>
         <Route path="/my-bookings" element={<MyBookings/>}/>
+        <Route path='/owner' element={<Layout/>}>
+          <Route index element={<Dashboard/>} />
+          <Route path="add-car" element={<AddCar/>} />
+          <Route path="manage-cars" element={<ManageCars/>} />
+          <Route path="manage-bookings" element={<ManageBookings/>} />
+        </Route>
       </Routes>
 
       {!isOwnerPath && <Footer/>}
