c24c8333f82261843a2ec5b8f5c57f7b091c514f
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-07-21 20:22:06 +0600

    add imageKit for uploading image and add the function to upload the data into database

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,4 +1,7 @@
+import imagekit from "../configs/imageKit.js";
+import Car from '../models/Car.js';
 import User from "../models/User.js";
+import fs from 'fs';
 
 export const changeRoleToOwner = async (req, res) => {
     try {
@@ -17,6 +20,28 @@ export const addCar = async (req, res) => {
         const {_id} = req.user;
         let car = JSON.parse(req.body.carData);
         const imageFile = req.file;
+
+        // upload image to image kit
+        const fileBuffer = fs.readFileSync(imageFile.path)
+        const response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder: '/cars'
+        });
+
+        // For URL Generation, works for both images and videos
+        var optimizedImageUrl = imagekit.url({
+            path : response.filePath,
+            transformation : [
+                {width: '1280'},
+                {quality: 'auto'},
+                {format: 'webp'},
+            ]
+        });
+        const image = optimizedImageUrl;
+        await Car.create({...car, owner: _id, image})
+        
+        res.json({success: true, message: "car added"})
     } catch (error) {
         console.log(error.message);
         res.json({success: false, message: error.message})
