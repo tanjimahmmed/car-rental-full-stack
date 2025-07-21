4dd733807ed0ed4cd221e6adeafd1ba4a1f4952f
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-07-21 14:09:42 +0600

    add owner addCar api with Car.js schema and add multer package for file management

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -51,3 +51,14 @@ export const loginUser = async (req, res) => {
         res.json({success: false, message: error.message})
     }
 }
+
+// Get user data using token (jwt)
+export const getUserData = async(req, res) => {
+    try {
+        const {user} = req;
+        res.json({success: true, user})
+    } catch(error){
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
